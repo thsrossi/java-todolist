aaf1815abfa0e563f229fe0c8146b0de3d23d38f
Rossinho <matheusbprossi@gmail.com>
2023-10-11 14:38:19 -0300

    using crypto to userPassword, created TaskModel

--- src/main/java/br/com/matheusrossidev/todolist/task/TaskModel.java
+++ src/main/java/br/com/matheusrossidev/todolist/task/TaskModel.java
@@ -1,4 +1,4 @@
-package br.com.matheusrossidev.todolist.user;
+package br.com.matheusrossidev.todolist.task;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,18 +12,22 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity(name="tb_users")
-public class UserModel {
-
+@Entity(name = "tb_tasks")
+public class TaskModel {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
+    private String description;
 
-    @Column(unique = true)
-    private String username;
-    private String name;
-    private String password;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+    
+    private UUID idUser;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
+    
 }
