5f044a2aee2529845d8d674eb93f2a5371656c43
unknown <matheusbprossi@gmail.com>
2023-10-11 21:15:56 -0300

    auth filter implemented

--- src/main/java/br/com/matheusrossidev/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/matheusrossidev/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.matheusrossidev.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.matheusrossidev.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -24,6 +30,30 @@ public class FilterTaskAuth extends OncePerRequestFilter{
 
                 byte[] authDecode = Base64.getDecoder().decode(authEncode);
 
-                filterChain.doFilter(request, response);
+                var authString = new String(authDecode);
+
+                String[] credentials = authString.split(":");
+
+                String username = credentials[0];
+
+                String password = credentials[1];
+
+                var user = this.userRepository.findByUsername(username);
+
+                if(user == null){
+                    response.sendError(401);
+                } else {
+
+                    var passwordVerify = BCrypt.verifyer()
+                    .verify(user.getPassword().toCharArray(), password.toCharArray());
+
+                    if(passwordVerify.verified){
+                        filterChain.doFilter(request, response);
+                    } else {
+                        response.sendError(401);
+                    }
+
+                }
+
             }
 }
